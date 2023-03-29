96aaef4a4d93634eaa84450333f2b21cf07b8f5a
jorozcop <orojacobo@gmail.com>
2023-03-29 17:09:14 -0500

    validaciones finales

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -185,7 +185,7 @@
       .col-lg-12(data-aos="fade-left")     
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)            
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/QdzC2JH61qY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)            
               
 
     .row.mt-3
